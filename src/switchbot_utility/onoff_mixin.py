5f8cad3edafbc9ebda29cc6b5ae344a2a01e8935
MATSUMURA Hidetoshi <icarrot@mac.com>
2026-03-04 09:10:39 +0900

    add Plug Mini(EU) and Curtain 3 classes

--- src/switchbot_utility/onoff_mixin.py
+++ src/switchbot_utility/onoff_mixin.py
@@ -3,12 +3,14 @@ class OnOffMixin:
 
     def turn_on(self) -> str:
         """Turn on device"""
+        self._body["commandType"] = "command"
         self._body["command"] = "turnOn"
         result = self.command(self.deviceId, self._body)
         return result.text
 
     def turn_off(self) -> str:
         """Turn off device"""
+        self._body["commandType"] = "command"
         self._body["command"] = "turnOff"
         result = self.command(self.deviceId, self._body)
         return result.text
