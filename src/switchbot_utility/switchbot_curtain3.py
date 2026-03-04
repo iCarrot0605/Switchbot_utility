38b6338ebf6bf231b5c3e88ef4bca66ad7409b35
MATSUMURA Hidetoshi <icarrot@mac.com>
2026-03-04 09:11:36 +0900

    add PlugMini

---
+++ src/switchbot_utility/switchbot_curtain3.py
@@ -0,0 +1,5 @@
+from switchbot_utility.switchbot_curtain import SwitchbotCurtain
+
+
+class SwitchbotCurtain3(SwitchbotCurtain):
+    """Switchbot Curtain 3 class"""
---
+++ src/switchbot_utility/switchbot_plug_mini_eu.py
@@ -0,0 +1,5 @@
+from switchbot_utility.switchbot_plug_mini_us import SwitchbotPlugMiniUS
+
+
+class SwitchbotPlugMiniEU(SwitchbotPlugMiniUS):
+    """Switchbot Plug Mini(EU) class"""
