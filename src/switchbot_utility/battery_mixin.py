63e3dba9fa1051262ae9a5e17ceda236932d09b7
MATSUMURA Hidetoshi <icarrot@mac.com>
2026-03-13 09:40:13 +0900

    Fix Battery level expression

--- src/switchbot_utility/battery_mixin.py
+++ src/switchbot_utility/battery_mixin.py
@@ -1,6 +1,9 @@
 class BatteryMixin:
     """Switchbot battery mixin"""
     def get_battery(self):
-        """Returns battery level"""
+        """Four-segment battery level division,<10%, shown as 10;
+        10%~20%, shown as 20;
+        20%~60%,shown as 60;
+        ≥60%, shown as 100"""
         status = self.get_status()
         return status["battery"]
