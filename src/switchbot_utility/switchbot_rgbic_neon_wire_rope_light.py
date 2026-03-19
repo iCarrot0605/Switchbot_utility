21e9c951cad29c8ffe4598888f2b4da28f5f117e
MATSUMURA Hidetoshi <icarrot@mac.com>
2026-03-19 10:05:18 +0900

    Added RGBICWW Strip light, RGBICWW Floor lamp, RGBIC Neon Wire Rope light and RGBIC Neon Wire Light

---
+++ src/switchbot_utility/switchbot_rgbic_neon_wire_rope_light.py
@@ -0,0 +1,6 @@
+from switchbot_utility.switchbot_ceiling_light import SwitchbotCeilingLight
+
+
+class SwitchbotRGBICNeonWireRopeLight(SwitchbotCeilingLight):
+    """Switchbot RGBIC Neon Wire Rope Light class"""
+
---
+++ src/switchbot_utility/switchbot_rgbicww_floor_lamp.py
@@ -0,0 +1,6 @@
+from switchbot_utility.switchbot_ceiling_light import SwitchbotCeilingLight
+
+
+class SwitchbotRGBICWWFloorLamp(SwitchbotCeilingLight):
+    """Switchbot RGBICWW Floor Lamp class"""
+
